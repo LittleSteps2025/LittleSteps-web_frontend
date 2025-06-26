e3de26e695979cdfe4ae62610d88738d56318746
Farshad135110 <farshadfahumy@gmail.com>
2025-06-27 01:06:28 +0530

    added api file

---
+++ src/api/api.ts
@@ -0,0 +1,14 @@
+// api.ts or wherever you call backend
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:5000/api"; // or your deployed backend URL
+
+export const getHello = async () => {
+  const res = await axios.get(`${API_BASE_URL}/hello`);
+  return res.data;
+};
+
+export const getUsers = async () => {
+  const res = await axios.get(`${API_BASE_URL}/users`);
+  return res.data;
+};
